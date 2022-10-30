96f820da49d40c8572d3dc556bbf14a14abb60d2
slapjd <slapjd@outlook.com>
2022-10-30 01:48:14 +0000

    Couple of sanity checks

--- src/users.ts
+++ src/users.ts
@@ -38,7 +38,6 @@ router.post("/", async function (req: Request, res: Response) {
     return res.send({message: "Success"})
 })
 
-//merge no make password go. need to figure that out
 router.put("/:id", async function (req: Request, res: Response) {
     if (req.params['id'] === undefined) return res.status(500).send({message: "USER ID EMPTY BUT ROUTED TO GET /:id"})
 
@@ -49,15 +48,24 @@ router.put("/:id", async function (req: Request, res: Response) {
         return res.status(404).send({ message: "User to modify does not exist" })
     }
     //mainDataSource.getRepository(User).merge(user, req.body)
-    user.merge(req.body)
+    //Add any properties that are allowed to be set on a user account settings change here
+    const safeBody = {
+        username: req.body['username'],
+        password: req.body['password']
+    }
+
+    //TODO: merge setters (maybe?)
+    user.merge(safeBody) //TODO: sanitize body if anything dangerous gets added to user
     const results = await mainDataSource.getRepository(User).save(user)
     return res.send(results)
 })
 
-//TODO: Authenticate with admin or make sure it's the same user
 router.delete("/:id", async function (req: Request, res: Response) {
     if (req.params['id'] === undefined) return res.status(500).send({message: "USER ID EMPTY BUT ROUTED TO GET /:id"})
+    if (!req.session.user) return res.status(401).send({message: "Login required"})
+    if (req.session.user.id != +req.params['id']) return res.status(403).send({message: "Not authorized to do this"}) //TODO: allow admins
 
+    //Checks passed, go ahead with the deletion
     const results = await mainDataSource.getRepository(User).delete(+req.params['id'])
     return res.send(results)
 })
