a761125f95d7bf171b58aa9f3c55a2d52fdb4222
slapjd <slapjd@outlook.com>
2022-11-06 23:46:39 +0000

    Login and logout dev button

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,8 +5,8 @@ import './assets/main.css'
 
 createApp(App).mount('#app')
 
-async function login() {
-    var test = await fetch('/api/auth/', {
+export async function login() {
+    var res = await fetch('/api/auth/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json; charset=utf-8'
@@ -17,7 +17,20 @@ async function login() {
         })
     })
 
-    alert(await test.json())
+    if (res.ok) alert("LOGIN SUCCESS")
+    else alert("LOGIN FAIL")
 }
 
-login()
+export async function logout() {
+    var res = await fetch('/api/auth/', {
+        method: 'DELETE'
+    })
+
+    if (res.ok) alert("LOGOUT SUCCESS")
+    else alert("LOGOUT FAIL")
+}
+
+window.onload = function() {
+    document.getElementById("loginButton").onclick = login
+    document.getElementById("logoutButton").onclick = logout
+}
