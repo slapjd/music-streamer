1dbade378c670a01226e423aeec76b69e6daeb62
slapjd <slapjd@outlook.com>
2022-11-24 20:55:52 +0000

    Let socket.io do the heavy lifting on host tracking

--- api/src/server/controllers/playback.io.controller.ts
+++ api/src/server/controllers/playback.io.controller.ts
@@ -1,54 +1,45 @@
 import type { Socket } from "socket.io"
-
-interface PlaybackSession {
-    host: Socket
-    remotes: Socket[]
-}
-
-const playbackSessions: PlaybackSession[] = []
+import { io } from "../../config/express"
 
 function connect(socket: Socket) {
-    socket.request.session.user = socket.request.session.user!
-    if (!playbackSessions[socket.request.session.user.id]) {
-        playbackSessions[socket.request.session.user.id] = {
-            host: socket,
-            remotes: []
-        }
+    const req = socket.request
+    req.session.user = req.session.user!
+
+    if (!io.sockets.adapter.rooms.get("USER" + req.session.user.id)) {
         socket.emit("becomeHost")
+        socket.join("HOST") //Pretty much just used so we know when the host quits
     } else {
-        playbackSessions[socket.request.session.user.id]!.remotes.push(socket)
         socket.emit("becomeRemote")
     }
 
-    socket.join(socket.request.session.id)
-    socket.join("USER" + socket.request.session.user.id)
+    socket.join(req.session.id) //Helpful when logouts are processed
+    socket.join("USER" + req.session.user.id) //Used for synchronizing playback
+
+    socket.onAny((event, ...args) => {
+        //Socket has been redefined here (i'm like pretty sure anyway, at least according to typescript)
+        const req = socket.request
+        req.session.user = req.session.user!
+
+        if (socket.rooms.has("HOST")) {
+            //Socket is host, do not send events back because it manages its own state sometimes and might get confused
+            socket.to("USER" + req.session.user.id).emit(event, args)
+        } else {
+            //Socket is remote and therefore stupid. It wants an event reply to tell it to *actually* do the requested action
+            io.to("USER" + req.session.user.id).emit(event, args) //This will send the event back to the sender
+        }
+    })
 }
 
-function disconnect(socket: Socket) {
-    //Lots of not-null assertions because typescript is dumb and we are smort
+async function disconnect(socket: Socket) {
+    //Lots of not-null assertions because typescript is dum and we are smort
+    //This is called *after* the socket has left all its rooms
     const req = socket.request
-
     req.session.user = req.session.user!
-    if (!playbackSessions[req.session.user.id]) {
-        console.warn("SOCKET_DISCONNECT_AFTER_SESSION_DELETE")
-        return //We do nothing because the session is already gone
-    }
 
-    const i = playbackSessions[req.session.user.id]!.remotes.findIndex(s => s == socket)
-
-    if (i) {
-        playbackSessions[req.session.user.id]!.remotes.splice(i, 1)
-    } else if (playbackSessions[req.session.user.id]!.host == socket) {
-        if (!playbackSessions[req.session.user.id]!.remotes[0]) {
-            delete playbackSessions[req.session.user.id]
-        } else {
-            playbackSessions[req.session.user.id]!.host = playbackSessions[req.session.user.id]!.remotes[0]!
-            playbackSessions[req.session.user.id]!.remotes.splice(0,1)
-        }
-    } else {
-        //Socket has session but is not part of playbackSession.
-        //dafuq?
-        throw "PLAYBACK_SESSION_PREMATURELY_DELETED_SEND_HELP"
+    if (!socket.rooms.has("HOST")) {
+        //Host has just disconnected, find a new host if possible.
+        var newHost = (await socket.to("USER" + req.session.user.id).fetchSockets()).find(() => true)
+        newHost?.join("HOST") //If no socket found, everyone has disconnected and host will be dealt with on next connection
     }
 }
 
